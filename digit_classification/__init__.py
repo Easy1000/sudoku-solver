63a64e323c6c2fbbee2877fae2aaaa0f35733b56
Easy1000 <izaaz240@gmail.com>
2025-09-13 17:42:35 +0700

    digit calssification: add init file

---
+++ digit_classification/__init__.py
@@ -0,0 +1,12 @@
+from .load_data import load_dataset
+from .model import (
+    build_model,
+    compile_model,
+    fit_model,
+    model_predict,
+    model_score,
+    resource_path,
+    save_model,
+)
+from .preprocess_data import prep_image, preprocess_dataset
+from .split_data import split_dataset
