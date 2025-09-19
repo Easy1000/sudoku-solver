c61ed6238562c4825e2793ff8596ec9e9e326c32
Easy1000 <izaaz240@gmail.com>
2025-09-19 21:52:48 +0700

    sudoku detection: add image wrap function
    - includes supporting functions such as reframe and main_outline
    - wrap and reframe the image to only show the puzzle

---
+++ sudoku_detection/wrap_puzzle.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+
+def image_wrap(contour, puzzle):
+    biggest, _ = main_outline(contour)
+    if biggest.size != 0:
+        biggest = reframe(biggest)
+        pts1 = np.float32(biggest)
+        pts2 = np.float32([[0, 0], [450, 0], [0, 450], [450, 450]])
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        image_wrap = cv2.warpPerspective(puzzle, matrix, (450, 450))
+        image_wrap = cv2.cvtColor(image_wrap, cv2.COLOR_BGR2GRAY)
+        return image_wrap
+
+    return None
+
+
+def reframe(points):
+    points = points.reshape((4, 2))
+    points_new = np.zeros((4, 1, 2), dtype=np.int32)
+    add = points.sum(1)
+    points_new[0] = points[np.argmin(add)]
+    points_new[3] = points[np.argmax(add)]
+    diff = np.diff(points, axis=1)
+    points_new[1] = points[np.argmin(diff)]
+    points_new[2] = points[np.argmax(diff)]
+    return points_new
+
+
+def main_outline(contour):
+    biggest = np.array([])
+    max_area = 0
+    for i in contour:
+        area = cv2.contourArea(i)
+        if area > 50:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+
+    return biggest, max_area
