0286f9ca5979be268cc50aeccd651fb053c1d41d
Easy1000 <izaaz240@gmail.com>
2025-09-19 21:55:40 +0700

    sudoku detection: rename preprocess function
    - includes renaming the preprocess_puzzle module to threshold_puzzle
    - renaming function to get_threshold

--- sudoku_detection/threshold_puzzle.py
+++ sudoku_detection/threshold_puzzle.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-def preprocess(image):
+def get_threshold(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (3, 3), 6)
     threshold = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
